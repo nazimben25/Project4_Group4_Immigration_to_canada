3ccde3c9ca421cf26713a00df6b393aa82274f21
nazimben25 <nazimbendjaballah@hotmail.com>
2025-02-26 20:59:39 -0500

    update the API with the new table commited before

--- api_app_project.py
+++ api_app_project.py
@@ -72,18 +72,19 @@ def welcome():
         f"  <br/>"
         f"Available Routes:<br/>"
         f"  <br/>"
-        f"List of the countries available     : /api/v0.1/countries_list <br/>"
+        f"List of the countries available in IRCC data    : /api/v0.1/countries_list_ircc <br/>"
         f"  <br/>"
-        f"List of the macro economic indicators available       : /api/v0.1/Macro_economic_indicators <br/>"
+        f"List of the countries coutries with additional information : region, cluster, lat, lon    : /api/v0.1/countries_data <br/>"
+        f"  <br/>"
+        f"List of the indicators available       : /api/v0.1/Macro_economic_indicators <br/>"
         f"  <br/>"
         f"immigation_flow_per_year_between 2015 and 2024       : /api/v0.1/immigation_flow_per_year_between/<year_start>/<year_end> <br/>"
+        f"  <br/>"
+        f"immigration_statistics_per_country (add the country code iso3 example : AGO for ANGLOA )       : /api/v0.1/immigration_statistics_per_country/country_select"
         f"  <br/>"
         f"immigation_flow_per_country       : /api/v0.1/immigation_flow_per_country <br/>"
         f"  <br/>"
         f"Immigration_statistics for all countries and all indicators       : /api/v0.1/immigration_statistics <br/>"
-        f"  <br/>"
-        f"immigration_statistics_per_country (add the country code iso3 example : AGO for ANGLOA )       : /api/v0.1/immigration_statistics_per_country/country_select"
-
 
         )
 
@@ -175,6 +176,56 @@ def Macro_economic_indicators():
 
 
 
+# # # route immigration_yearly : returns the cumulated immigration flow per year - between to year (2015 and 2024)
+
+# @app.route("/api/v0.1/immigation_flow_per_year_between/<year_start>/<year_end>")
+# def immigation_flow_per_year_between(year_start, year_end):
+
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
+
+#     # query measurment table for the last date in the DB
+
+# # set a condition to be sure that dates are consistant
+
+#     if year_end >= year_start :
+
+#         # grop by table measurment, and calculate TMIN, TAVG, TMAX) for each date after filter of dates              
+#         result = session.query(immigration.country, func.sum(immigration.immigration_flow)\
+#                         .filter(immigration.year >= year_start)\
+#                         .filter(immigration.year <= year_end))\
+#                         .group_by(immigration.country) \
+#                         .order_by(func.sum(immigration.immigration_flow).desc()) \
+#                         .all()
+
+#         # close session
+#         session.close()        
+        
+#         #create a list to be jsonified, by loopong through the result above
+ 
+#         flow_by_country_list = []
+#         for country, sumflow in result:
+#             flow_dict = {}
+#             flow_dict['description'] = f'Immigration flow for the period between {year_start} and {year_end}'
+#             flow_dict['country'] = country
+#             flow_dict['flow'] = sumflow
+
+
+#             flow_by_country_list.append(flow_dict)
+
+#         return jsonify(flow_by_country_list)
+    
+#     else : 
+#         # error message if dates are not consistant
+#         return 'NEIN!!! DAS IST NICH GUT !!! <br/> \
+#             <br/> \
+#             नहीं!!! यह अच्छा नहीं है <br/> \
+#             <br/> \
+#             NO!!! ESO NO ES BUENO <br/> \
+#             <br/> \
+#             Year_end must be later than Year_start. <br/> \
+#             In other words, you must enter a "year_end" that is after the "year_start".' 
+
 # # route immigration_yearly : returns the cumulated immigration flow per year - between to year (2015 and 2024)
 
 @app.route("/api/v0.1/immigation_flow_per_year_between/<year_start>/<year_end>")
@@ -183,19 +234,32 @@ def immigation_flow_per_year_between(year_start, year_end):
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    print(f'Sunil is the best or is it felipe')
     # query measurment table for the last date in the DB
 
-# set a condition to be sure that dates are consistant
+    # set a condition to be sure that dates are consistant
 
     if year_end >= year_start :
 
+        # Create a subquery for macrodata
+        macrodata_subquery = session.query(
+                                            macrodata.country.label("country"),
+                                            func.sum(macrodata.value).label("flow_100k")) \
+        .filter(macrodata.year >= year_start)\
+        .filter(macrodata.year <= year_end)\
+        .filter(macrodata.indicator == 'immigration_100k') \
+        .group_by(macrodata.country).subquery()
+
         # grop by table measurment, and calculate TMIN, TAVG, TMAX) for each date after filter of dates              
-        result = session.query(immigration.country, func.sum(immigration.immigration_flow)\
+        result = session.query(
+                                immigration.country, 
+                                func.sum(immigration.immigration_flow).label("flow"),\
+                                macrodata_subquery.c.flow_100k) \
                         .filter(immigration.year >= year_start)\
-                        .filter(immigration.year <= year_end))\
+                        .filter(immigration.year <= year_end)\
                         .group_by(immigration.country) \
                         .order_by(func.sum(immigration.immigration_flow).desc()) \
+                        .join(
+                               macrodata_subquery, immigration.country == macrodata_subquery.c.country)\
                         .all()
 
         # close session
@@ -204,11 +268,12 @@ def immigation_flow_per_year_between(year_start, year_end):
         #create a list to be jsonified, by loopong through the result above
  
         flow_by_country_list = []
-        for country, sumflow in result:
+        for country, sumflow, sumflow_k in result:
             flow_dict = {}
-            flow_dict['description'] = f'Immigration flow for the period between {year_start} and {year_end}'
+            flow_dict['description'] = f'Immigration flows for the period between {year_start} and {year_end}'
             flow_dict['country'] = country
             flow_dict['flow'] = sumflow
+            flow_dict['flow_100k'] = sumflow_k
 
 
             flow_by_country_list.append(flow_dict)
@@ -225,43 +290,6 @@ def immigation_flow_per_year_between(year_start, year_end):
             <br/> \
             Year_end must be later than Year_start. <br/> \
             In other words, you must enter a "year_end" that is after the "year_start".' 
-
-
-
-# @app.route("/api/v0.1/immigration_statistics")
-# def immigration_statistics():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
-
-#     # Create a subquery for macrodata
-#     macrodata_subquery = session.query(
-#         macrodata.iso3Code.label("macro_iso3Code"),
-#         macrodata.indicator.label("indicator"),
-#         func.avg(macrodata.value).label("avg_value")
-#         ).group_by(macrodata.iso3Code, macrodata.indicator).subquery()
-
-#     # Create a subquery for immigration data
-#     immigration_subquery = session.query(
-#         immigration.country.label("country"),
-#         func.sum(immigration.immigration_flow).label("imm_flow")
-#     ).group_by(immigration.country).subquery()
-
-#     # Main query
-#     result = session.query(
-#         immigration_subquery.c.country,
-#         immigration_subquery.c.imm_flow,
-#         countries.iso3Code.label("country_iso3Code"),
-#         countries.region,
-#         countries.latitude,
-#         countries.longitude,
-#         macrodata_subquery.c.indicator,
-#         macrodata_subquery.c.avg_value
-#     ).join(
-#         countries, immigration_subquery.c.country == countries.country
-#     ).join(
-#         macrodata_subquery, countries.iso3Code == macrodata_subquery.c.macro_iso3Code
-#     ).all()
-
     
 @app.route("/api/v0.1/immigration_statistics_per_country/<country_select>")
 def immigration_statistics_per_country(country_select):
@@ -320,6 +348,43 @@ def immigration_statistics_per_country(country_select):
         flow_by_country_list.append(flow_dict)
 
     return jsonify(flow_by_country_list)
+
+ 
+
+# @app.route("/api/v0.1/immigration_statistics")
+# def immigration_statistics():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
+
+#     # Create a subquery for macrodata
+#     macrodata_subquery = session.query(
+#         macrodata.iso3Code.label("macro_iso3Code"),
+#         macrodata.indicator.label("indicator"),
+#         func.avg(macrodata.value).label("avg_value")
+#         ).group_by(macrodata.iso3Code, macrodata.indicator).subquery()
+
+#     # Create a subquery for immigration data
+#     immigration_subquery = session.query(
+#         immigration.country.label("country"),
+#         func.sum(immigration.immigration_flow).label("imm_flow")
+#     ).group_by(immigration.country).subquery()
+
+#     # Main query
+#     result = session.query(
+#         immigration_subquery.c.country,
+#         immigration_subquery.c.imm_flow,
+#         countries.iso3Code.label("country_iso3Code"),
+#         countries.region,
+#         countries.latitude,
+#         countries.longitude,
+#         macrodata_subquery.c.indicator,
+#         macrodata_subquery.c.avg_value
+#     ).join(
+#         countries, immigration_subquery.c.country == countries.country
+#     ).join(
+#         macrodata_subquery, countries.iso3Code == macrodata_subquery.c.macro_iso3Code
+#     ).all()
+   
 
    
     
